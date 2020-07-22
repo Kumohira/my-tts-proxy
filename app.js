ca355e8a52ddebc224919890f0c630934ed17151
kumohira <naitsaid.youssef@gmail.com>
2020-07-22 05:40:35 +0100

    fix request with special characters

--- app.js
+++ app.js
@@ -12,13 +12,15 @@ app.get('/my-proxy', (req, res) => {
   const voice = req.param('voice');
   const text = req.param('text');
 
-  let url = 'https://streamlabs.com/polly/speak?voice=' + voice + '&text=' + text;
+  const url = 'https://streamlabs.com/polly/speak?voice=' + voice + '&text=' + text;
+  const encodedURL = encodeURI(url);
 
   request.post(
-    { url: url },
+    { url: encodedURL },
     (error, response, body) => {
       if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: response.body });
+        const customError = error || response.body;
+        return res.status(500).json({ type: 'error', message: customError });
       }
       res.json(JSON.parse(body));
     }
